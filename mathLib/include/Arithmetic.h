ddadd422e6b8a743f6f4e2b168a18d112d533d1e
David Henry <user@example.com>
2023-02-12 21:13:43 -0500

    cleanup

--- mathLib/include/Arithmetic.h
+++ mathLib/include/Arithmetic.h
@@ -1,9 +1,19 @@
-#include <iostream>
+#ifndef DVID_TEST_CPP_LIB_ONE_ARITHMETIC_H_
+#define DVID_TEST_CPP_LIB_ONE_ARITHMETIC_H_
 
 namespace dvid {
 namespace arithmetic {
 
+/**
+ * Test addition method. Doesn't test for overflow or underflow (eg. -INT_MAX + -INT_MAX).
+ *
+ * @param a
+ * @param b
+ * @return a + b
+ */
 int add(int a, int b);
 
 } // namespace arithmetic
 } // namespace dvid
+
+#endif
